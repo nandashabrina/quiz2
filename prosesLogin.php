643aff34dc11f2e2c96e6f848d391db377806e58
nandashabrina <2131710064@student.polinema.ac.id>
2022-05-18 18:38:25 +0700

    commit changes

--- prosesLogin.php
+++ prosesLogin.php
@@ -9,15 +9,10 @@
     $check = mysqli_num_rows($result);
 
     if($check>0){
-        header("Location:adminPage.php");
-        if($username == "admin1" && $password == "123" || $username == "admin2" && $password == "234"){
             session_start();
             $_SESSION["username"] = $username;
             $_SESSION["role"] = "admin";
             header("Location:adminPage.php");
-        }else{
-            echo "Incorrect username or password, please try again!";
-        }
     }
     else{
         header("Location:formLogin.php?error=gagal");
